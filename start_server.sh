7b3648be8704d2858d82db3d591dc53fe0b9502c
Stin_ssu <43136526+jimmy3663@users.noreply.github.com>
2022-05-31 09:30:34 +0900

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,3 @@
 echo "start application server"
+unzip reqres_products
 nohup java -Dreqres_products-app -jar /home/ubuntu/reqres_products/reqres_products-0.0.1-SNAPSHOT.jar > /home/ubuntu/reqres_products/nohup.out 2>&1 &
